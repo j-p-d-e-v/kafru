35874448458a69285900af254711fa335879e42b
JP <jp@tiktoklive.com>
2024-09-09 00:54:11 +0800

    Added test case for advance schedule

--- src/tests/test_manager.rs
+++ src/tests/test_manager.rs
@@ -76,4 +76,43 @@ mod test_manager {
         let _ = manager.scheduler("kafru_test_scheduler".to_string(), 5).await;
         let _ = manager.join().await;
     }
+
+    
+    #[tokio::test]
+    async fn test_advance(){
+        configure_database_env();
+
+        let schedule: Schedule = Schedule::new().await;
+        let mut manager: Manager = Manager::new().await;
+
+        // Purge records
+        let result = schedule.purge().await;
+        assert!(result.is_ok(),"{:?}",result.unwrap_err());
+
+        //Create sample schedules
+        for i in 1..11 {
+            let result: Result<ScheduleData, String> = schedule.create(ScheduleData {
+                name: Some(format!("{} - {}",i,Name().fake::<String>())),
+                queue: Some("default".to_string()),
+                cron_expression: Some(CronSchedule::new().set_minute(format!("*/{}",i))),
+                handler:Some("mytesthandler".to_string()),
+                start_schedule: Utc::now().checked_add_days(Days::new(1)),
+                until_schedule: Utc::now().checked_add_days(Days::new(3)),
+                one_time: if i % 2 == 0 { true } else { false },
+                status: Some(ScheduleStatus::Enabled),
+                parameters: Some(HashMap::from([
+                    ( "schedule1".to_string(), Value::String(Sentence( Range {start: 1, end: 5 }).fake::<String>()) ),
+                    ( "schedule2".to_string(), Value::Number(Number::from(123)) )
+                ])),
+                ..Default::default()
+            }).await;
+            assert!(result.is_ok(),"{}",result.unwrap_err());
+        }
+        let mut task_registry: TaskRegistry = TaskRegistry::new().await;
+        task_registry.register("mytesthandler".to_string(), || Box::new(MyTestStructA { message: "Hello World".to_string() })).await;
+        let task_registry: Arc<TaskRegistry> = Arc::new(task_registry);
+        let _ = manager.worker("default".to_string(), 5, task_registry.clone(), 15).await;
+        let _ = manager.scheduler("kafru_test_scheduler".to_string(), 5).await;
+        let _ = manager.join().await;
+    }
 }
